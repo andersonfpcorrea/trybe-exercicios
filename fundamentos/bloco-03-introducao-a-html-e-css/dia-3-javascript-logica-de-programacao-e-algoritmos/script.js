0777976c202967a1e4bacc05ba94eb6a5438f290
Anderson Correa <andersonfpcorrea@gmail.com>
2022-04-08 18:06:08 -0300

    2nd exercise

--- fundamentos/bloco-03-introducao-a-html-e-css/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-03-introducao-a-html-e-css/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -7,18 +7,38 @@
 //            into another variable ("x", for instance), and then for the following
 //            iterations makes x = x (inputNumber - i), being i the present iteration
 //            (or repetition).
-const inputNumber = Number(
-  prompt(`Wite down any number and we will calculate its factorial 
-for you :) - Write only numbers, do not use letters!`)
-);
-let sum = inputNumber;
-for (let i = 1; i < inputNumber; i += 1) {
-  sum = sum * (inputNumber - i);
-}
-console.log(sum);
+// const inputNumber = Number(
+//   prompt(`Wite down any number and we will calculate its factorial
+// for you :) - Write only numbers, do not use letters!`)
+// );
+// let sum = inputNumber;
+// for (let i = 1; i < inputNumber; i += 1) {
+//   sum = sum * (inputNumber - i);
+// }
+// console.log(sum);
 
 // 2. Create an algorithm that invert the letter of a word;
+// Answer:
+//  1. First, given any input word, we create an object (myStringObject) from this
+//     string with "new String"
+//  2. We iterate through the string object and save its letters, from the last to the first
+//     into a new array (invertStringArray)
+//  3. We use the join('') method to create a new string by concatenating all of the elements of
+//     "invertStringArray", and we save this string to a new variable (myString2)
+const myStringObject = new String("Anderson");
+const invertStringArray = [];
+let myString2;
+for (let i = 1; i <= myStringObject.length; i += 1) {
+  invertStringArray.push(myStringObject[myStringObject.length - i]);
+}
+myString2 = invertStringArray.join("");
+
+console.log(myStringObject);
+console.log(invertStringArray);
+console.log(myString2);
+
 // 3. `let array = ['java', 'javascript', 'python', 'html', 'css'];`
 //    Write two algorithms: one that return a largest word of `array` e another that return
 //    the smaller.
+
 // 4. Write an algorithm that return the biggest odd number between 0 and 50.
