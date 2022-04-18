f6c85a6ae4ebc1a6be3659d75cff17290b7a775a
Anderson Correa <andersonfpcorrea@gmail.com>
2022-04-18 19:36:49 -0300

    exercise 1

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -1,22 +1,56 @@
-window.onload = jsCarregado;
+function createDaysOfTheWeek() {
+  const weekDays = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ];
+  const weekDaysList = document.querySelector(".week-days");
 
-function jsCarregado() {
-  alert("Página foi carregada!");
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement("li");
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  }
 }
 
-function maisInfos() {
-  let textoMaisInfo = document.getElementById("trybe-continua");
-  textoMaisInfo.innerText =
-    "Aqui você vai aprender Hard Skills, Soft Skills e várias outras habilidades";
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+
+// Exercise 1
+// Create a function that generate each calendar day and add them as child element of the
+// tag `ul` with the ID=days.
+// All days must have the class 'day'.
+// The days 24, 25 and 31 must have the aditional class 'holiday'.
+// The days 4, 11, 18 and 25 must have the aditional class 'friday'.
+const dezDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
+
+function createDaysOfTheMonth() {
+  const monthDaysList = document.getElementById("days");
+
+  for (i = 0; i < dezDaysList.length; i += 1) {
+    const monthDay = document.createElement("li");
+    monthDaysList.appendChild(monthDay).textContent = `${dezDaysList[i]}`;
+    // Implementation of the classes to the days:
+    if (dezDaysList[i] === 24 || dezDaysList[i] === 25 || dezDaysList[i] === 31)
+      document.querySelectorAll("#days li")[i].className = "day holiday";
+    else if (
+      dezDaysList[i] === 4 ||
+      dezDaysList[i] === 11 ||
+      dezDaysList[i] === 18 ||
+      dezDaysList[i] === 25
+    )
+      document.querySelectorAll("#days li")[i].className = "day friday";
+    else document.querySelectorAll("#days li")[i].classList.add("day");
+  }
 }
-
-let clickP = document.getElementById("clicar");
-
-clickP.addEventListener("click", function recebeClique(eventoDeOrigem) {
-  // Objeto evento
-  // As duas principais propriedades:
-  // Target: o elemento que originou o evento
-  // Type: o tipo de evento
-  console.log(eventoDeOrigem.target);
-  console.log(eventoDeOrigem.type);
-});
+createDaysOfTheMonth();
